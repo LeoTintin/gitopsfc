5e91e2c1d52711156c3bf9ff09c4031a76cdcd8c
Leonardo <l.guerarti@gmail.com>
2025-07-23 15:56:24 -0300

    feat: mundando mensagem de hello

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("<h1>Hello, Argo!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
